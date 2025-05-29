5a558fe03486407c5349399d2d8f405ab79bd6a2
Abid064 <abidkhan09989@gmail.com>
2025-05-29 18:32:39 +0600

    env functionality done

--- src/firebase/firebase.init.js
+++ src/firebase/firebase.init.js
@@ -6,12 +6,12 @@ import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyB19ProzP7nRtQyKnaxIO8rbkgIO_o_DuY",
-  authDomain: "chill-game-687dd.firebaseapp.com",
-  projectId: "chill-game-687dd",
-  storageBucket: "chill-game-687dd.firebasestorage.app",
-  messagingSenderId: "377598491650",
-  appId: "1:377598491650:web:c25999d15a7ad014597b1b",
+  apiKey: import.meta.env.VITE_apiKey,
+  authDomain: import.meta.env.VITE_authDomain,
+  projectId: import.meta.env.VITE_projectId,
+  storageBucket: import.meta.env.VITE_storageBucket,
+  messagingSenderId: import.meta.env.VITE_messagingSenderId,
+  appId: import.meta.env.VITE_appId,
 };
 
 // Initialize Firebase
